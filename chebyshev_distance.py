a0b456a4f8f0acac4559ee4aab2da621e13e5622
ATOPIA-Sandra <94612ayms@gmail.com>
2024-05-17 18:48:30 +0000

    second implementation of the function chebyshev_distance

--- chebyshev_distance.py
+++ chebyshev_distance.py
@@ -1,3 +1,16 @@
+#...............................exercice 40.........................
+#first implementation of the function chebyshev_distance
 def chebyshev_distance(point1, point2):
     return max(abs(a-b) for a, b in zip(point1,point2))
+
+
+
+#second implementation of the function chebyshev_distance
+
+def chebyshev_distance(point1, point2):
+    max_distance =0
+    for a, b in zip(point1, point2):
+        max_distance = max(max_distance, abs(a-b))
+    return max_distance
+
 
